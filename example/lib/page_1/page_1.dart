44579f111659c30bf7b716b1cde3b9aa97937321
VNNiz <contact@niz.vn>
2023-04-10 02:09:54 +0700

    Update example

--- example/lib/page_1/page_1.dart
+++ example/lib/page_1/page_1.dart
@@ -5,3 +5,11 @@ final text2 = 'This is a "quoted" string 1'.trT();
 final text3 = "This is a 'quoted' string 1".trP({'param': 'value'});
 final text4 =
     'This is a string with @{num} parameters 1'.trF(params: {'num': 2});
+
+final text5 =
+    "Hello, wsa fa;senf jb awbdfb hkbkahbfwlbljbwa bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1"
+        .tr;
+
+final text6 = 'Hello, wsa fa;senf jb awbdfrr b hkbkahbfwlbljbwa '
+        'bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1'
+    .tr;
--- example/lib/resources/language_helper/_language_data_abstract.g.dart
+++ example/lib/resources/language_helper/_language_data_abstract.g.dart
@@ -10,36 +10,38 @@ const analysisLanguageData = {
   ///==============================================
   /// Path: page_1/page_1.dart
   ///==============================================
-  "Hello, world! 1": "Hello, world! 1",
   'This is a "quoted" string 1': 'This is a "quoted" string 1',
-  "This is a 'quoted' string 1": "This is a 'quoted' string 1",
   'This is a string with @{num} parameters 1': 'This is a string with @{num} parameters 1',
+  "This is a 'quoted' string 1": "This is a 'quoted' string 1",
+  "Hello, world! 1": "Hello, world! 1",
+  "Hello, wsa fa;senf jb awbdfb hkbkahbfwlbljbwa bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1": "Hello, wsa fa;senf jb awbdfb hkbkahbfwlbljbwa bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1",
+  'Hello, wsa fa;senf jb awbdfrr b hkbkahbfwlbljbwa '        'bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1': 'Hello, wsa fa;senf jb awbdfrr b hkbkahbfwlbljbwa '        'bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1',
 
   ///==============================================
   /// Path: page_1/page_2.dart
   ///==============================================
-  // "Hello, world! 1": "Hello, world! 1",
   // 'This is a "quoted" string 1': 'This is a "quoted" string 1',
-  "This is a 'quoted' string 2": "This is a 'quoted' string 2",
   'This is a string with @{num} parameters 2': 'This is a string with @{num} parameters 2',
+  "This is a 'quoted' string 2": "This is a 'quoted' string 2",
+  // "Hello, world! 1": "Hello, world! 1",
 
   ///==============================================
   /// Path: page_2/page_1.dart
   ///==============================================
-  "Hello, world! 2": "Hello, world! 2",
   'This is a "quoted" string 2': 'This is a "quoted" string 2',
-  "This is a 'quoted' string 3": "This is a 'quoted' string 3",
   'This is a string with @{num} parameters 3': 'This is a string with @{num} parameters 3',
+  "This is a 'quoted' string 3": "This is a 'quoted' string 3",
+  "Hello, world! 2": "Hello, world! 2",
 
   ///==============================================
   /// Path: page_2/page_2.dart
   ///==============================================
-  "Hello, world! 3": "Hello, world! 3",
   'This is a "quoted" string 3': 'This is a "quoted" string 3',
-  "This is a 'quoted' string 4": "This is a 'quoted' string 4",
   'This is a string with @{num} parameters 4': 'This is a string with @{num} parameters 4',
-  'This is a "quoted" string 5': 'This is a "quoted" string 5',
-  // 'This is a "quoted" string 5': 'This is a "quoted" string 5',
+  "This is a 'quoted' string 4": "This is a 'quoted' string 4",
+  "Hello, world! 3": "Hello, world! 3",
   "This is a 'quoted' string 5": "This is a 'quoted' string 5",
   // "This is a 'quoted' string 5": "This is a 'quoted' string 5",
+  'This is a "quoted" string 5': 'This is a "quoted" string 5',
+  // 'This is a "quoted" string 5': 'This is a "quoted" string 5',
 };
