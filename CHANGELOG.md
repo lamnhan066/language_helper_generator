7d2c5c1fa696185b2b0f420b2c067773bf3ab325
VNNiz <contact@niz.vn>
2023-04-10 02:10:08 +0700

    Update CHANGELOG, version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-## 0.0.1-rc1
+## 0.0.1-rc.2
+
+* User new parser to parse string which supports multiple lines.
+
+## 0.0.1-rc.1
 
 * Initial release.
--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'package:language_helper_generator/src/generator.dart';
+import 'package:language_helper_generator/language_helper_generator.dart';
 
 void main(List<String> args) async {
   final generator = LanguageHelperGenerator();
