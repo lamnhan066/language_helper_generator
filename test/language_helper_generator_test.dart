22ac1774d50d8fbe526fd0c0ec29cab23ff36a99
VNNiz <contact@niz.vn>
2023-04-10 12:26:37 +0700

    Add tests

--- test/language_helper_generator_test.dart
+++ test/language_helper_generator_test.dart
@@ -1,1 +1,171 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:language_helper_generator/language_helper_generator.dart';
+import 'package:language_helper_generator/src/models/data_type.dart';
+
+void main() {
+  final generator = LanguageHelperGenerator();
+
+  group('Test parser', () {
+    test('Parse single line', () {
+      const text = '''
+        final tr = 'This is a "test" text'.tr;
+        final tr1 = "This is a 'test' text".tr;
+
+        final tr = 'This is a "test" text'.trF();
+        final tr1 = "This is a 'test' text".trF();
+
+        final tr = 'This is a "test" text'.trT();
+        final tr1 = "This is a 'test' text".trT();
+
+        final tr = 'This is a "test" text'.trP();
+        final tr1 = "This is a 'test' text".trP();
+
+      ''';
+      final result = generator.parse(text);
+
+      expect(
+          result.map((e) => e.text),
+          containsAll([
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+          ]));
+    });
+
+    test('Parse multiple lines', () {
+      const text = '''
+        final tr = 'This is ' 'a "test" '
+            'text'.tr;
+        final tr1 = "This is ' 'a 'test' '
+            'text".tr;
+
+        final tr = 'This is ' 'a "test" text'
+            .trF();
+        final tr1 = "This is a "
+            "'test' text"
+            .trF();
+
+        final tr = 'This is a "test" '
+            'text'.trT();
+        final tr1 = "This "
+            "is a 'test' text".trT();
+
+        final tr = 'This ' 'is a "test" ' 'text'.trP();
+        final tr1 = "This ""is a 'test' ""text".trP();
+
+      ''';
+      final result = generator.parse(text);
+
+      expect(
+          result.map((e) => e.text),
+          containsAll([
+            """'This is a "test" '            'text'""",
+            '''"This "            "is a 'test' text"''',
+            """'This is ' 'a "test" text'""",
+            '''"This is a "            "'test' text"''',
+            """'This ' 'is a "test" ' 'text'""",
+            '''"This ""is a 'test' ""text"''',
+            """'This is ' 'a "test" '            'text'""",
+            '''"This is ' 'a 'test' '            'text"''',
+          ]));
+    });
+
+    test('Parse `translate` method single line', () {
+      const text = '''
+        final tr = languageHelper.translate('This is a "test" text');
+        final tr1 = languageHelper.translate("This is a 'test' text");
+
+        final tr = languageHelper.translate('This is a "test" text');
+        final tr1 = languageHelper.translate("This is a 'test' text");
+
+        final tr = languageHelper.translate('This is a "test" text');
+        final tr1 = languageHelper.translate("This is a 'test' text");
+
+        final tr = languageHelper.translate('This is a "test" text');
+        final tr1 = languageHelper.translate("This is a 'test' text");
+
+      ''';
+      final result = generator.parse(text);
+
+      expect(
+          result.map((e) => e.text),
+          containsAll([
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+            """'This is a "test" text'""",
+            '''"This is a 'test' text"''',
+          ]));
+    });
+
+    test('Parse `translate` method multiple lines', () {
+      const text = '''
+        final tr = languageHelper.translate('This is ' 'a "test" '
+            'text');
+        final tr1 = languageHelper.translate("This is ' 'a 'test' '
+            'text");
+
+        final tr = languageHelper.translate('This is ' 'a "test" text'
+            );
+        final tr1 = languageHelper.translate("This is a "
+            "'test' text"
+            );
+
+        final tr = languageHelper.translate('This is a "test" '
+            'text');
+        final tr1 = languageHelper.translate("This "
+            "is a 'test' text");
+
+        final tr = languageHelper.translate('This ' 'is a "test" ' 'text');
+        final tr1 = languageHelper.translate("This ""is a 'test' ""text");
+
+      ''';
+      final result = generator.parse(text);
+
+      expect(
+          result.map((e) => e.text),
+          containsAll([
+            """'This is a "test" '            'text'""",
+            '''"This "            "is a 'test' text"''',
+            """'This is ' 'a "test" text'""",
+            '''"This is a "            "'test' text"''',
+            """'This ' 'is a "test" ' 'text'""",
+            '''"This ""is a 'test' ""text"''',
+            """'This is ' 'a "test" '            'text'""",
+            '''"This is ' 'a 'test' '            'text"''',
+          ]));
+    });
+
+    test('Parse ignoring variable', () {
+      const text = '''
+        final tr = languageHelper.translate('This is ' 'a "test" '
+            'text with \$variable');
+        final tr1 = languageHelper.translate('This is ' 'a "test" '
+            'text with \${variable.1}');
+        final tr2 = languageHelper.translate(\$variable);
+        
+        final tr3 = 'This is ' 'a "test" '
+            'text with \$variable'.tr;
+        final tr4 = 'This is ' 'a "test" '
+            'text with \${variable.1}'.tr;
+        final tr5 = variable.tr;
+      ''';
+      final result = generator.parse(text);
+
+      for (final parsed in result) {
+        expect(
+          parsed.type,
+          equals(DataType.containsVariable),
+        );
+      }
+    });
+  });
+}
