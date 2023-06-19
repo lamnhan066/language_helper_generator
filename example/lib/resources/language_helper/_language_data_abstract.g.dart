4ecd02d05b3b596b3ecd187a0dc0740d68398b54
VNNiz <contact@niz.vn>
2023-06-19 08:31:27 +0700

    * Update the generated files in the example.

--- example/lib/resources/language_helper/_language_data_abstract.g.dart
+++ example/lib/resources/language_helper/_language_data_abstract.g.dart
@@ -1,8 +1,11 @@
 //==========================================================
-// Author: Lâm Thành Nhân (2023)
+// Author: Lâm Thành Nhân
+// Generated Date: 2023-06-19 08:26:37.075989 (Local)
 //
 // Generated Code - Do not modify by hand
 //==========================================================
+
+// ignore_for_file: prefer_single_quotes
 
 part of 'language_data.dart';
 
@@ -16,15 +19,21 @@ const analysisLanguageData = {
   "Hello, world! 1": "Hello, world! 1",
   "Hello, wsa fa;senf jb awbdfb hkbkahbfwlbljbwa bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1": "Hello, wsa fa;senf jb awbdfb hkbkahbfwlbljbwa bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1",
   'Hello, wsa fa;senf jb awbdfrr b hkbkahbfwlbljbwa '        'bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1': 'Hello, wsa fa;senf jb awbdfrr b hkbkahbfwlbljbwa '        'bjfbenqbfolaejbcfoj oljbnjafjlbqorld! 1',
-  // 'This text contains variable $text7': 'This text contains variable $text7',  // contains DataType.containsVariable
+  // 'This text contains variable $text7': 'This text contains variable $text7',  // Commented reason: Contains variable
 
   ///==============================================
   /// Path: page_1/page_2.dart
   ///==============================================
-  // 'This is a "quoted" string 1': 'This is a "quoted" string 1',  // Duplicated
+  // 'This is a "quoted" string 1': 'This is a "quoted" string 1',  // Commented reason: Duplicated
   'This is a string with @{num} parameters 2': 'This is a string with @{num} parameters 2',
   "This is a 'quoted' string 2": "This is a 'quoted' string 2",
-  // "Hello, world! 1": "Hello, world! 1",  // Duplicated
+  // "Hello, world! 1": "Hello, world! 1",  // Commented reason: Duplicated
+
+  ///==============================================
+  /// Path: utils/language_helper_mock.dart
+  ///==============================================
+  'result is something': 'result is something',
+  'result is @{param}': 'result is @{param}',
 
   ///==============================================
   /// Path: page_2/page_1.dart
@@ -42,7 +51,7 @@ const analysisLanguageData = {
   "This is a 'quoted' string 4": "This is a 'quoted' string 4",
   "Hello, world! 3": "Hello, world! 3",
   'This is a "quoted" string 5': 'This is a "quoted" string 5',
-  // 'This is a "quoted" string 5': 'This is a "quoted" string 5',  // Duplicated
+  // 'This is a "quoted" string 5': 'This is a "quoted" string 5',  // Commented reason: Duplicated
   "This is a 'quoted' string 5": "This is a 'quoted' string 5",
-  // "This is a 'quoted' string 5": "This is a 'quoted' string 5",  // Duplicated
+  // "This is a 'quoted' string 5": "This is a 'quoted' string 5",  // Commented reason: Duplicated
 };
