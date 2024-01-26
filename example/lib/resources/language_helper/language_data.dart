e0b5ac8be33cecb3b7291f330086f09b19536754
lamnhan066 <hello@lamnhan.dev>
2024-01-26 23:22:37 +0700

    Update example

--- example/lib/resources/language_helper/language_data.dart
+++ example/lib/resources/language_helper/language_data.dart
@@ -1,10 +1,10 @@
 import 'package:language_helper/language_helper.dart';
 
-part '_language_data_abstract.g.dart';
+part 'languages/_generated.dart';
 
 final languageHelper = LanguageHelper.instance;
 
 LanguageData languageData = {
-  // TODO: You can use this data as your main language, remember to change this code to your base language code
+  // TODO: You can use this data as your main language, remember to change [LanguageCodes.en] to your base language code
   LanguageCodes.en: analysisLanguageData,
 };
