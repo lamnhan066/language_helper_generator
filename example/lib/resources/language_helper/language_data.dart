cea49e3000122fdb4bedee66bb12796e62544e1d
Lam Thanh Nhan <hello@lamnhan.dev>
2025-10-23 18:25:49 +0700

    refactor: use explicit en/vi language data

--- example/lib/resources/language_helper/language_data.dart
+++ example/lib/resources/language_helper/language_data.dart
@@ -1,10 +1,9 @@
 import 'package:language_helper/language_helper.dart';
 
-part 'languages/_generated.dart';
-
-final languageHelper = LanguageHelper.instance;
+import 'languages/en.dart';
+import 'languages/vi.dart';
 
 LanguageData languageData = {
-  // TODO: You can use this data as your main language, remember to change [LanguageCodes.en] to your base language code
-  LanguageCodes.en: analysisLanguageData,
+  LanguageCodes.en: enLanguageData,
+  LanguageCodes.vi: viLanguageData,
 };
