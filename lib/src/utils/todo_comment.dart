772981c458217a4873971239c6b22a4871cd8aa1
Lam Thanh Nhan <hello@lamnhan.dev>
2025-10-24 11:31:20 +0700

    docs: add doc comments for todo utilities

--- lib/src/utils/todo_comment.dart
+++ lib/src/utils/todo_comment.dart
@@ -1,3 +1,5 @@
+/// Generate the `TODO` comment
 String todoComment(String code) => '// TODO: Add `$code` language translation';
 
+/// Check if [line] contains `TODO`
 bool containsTodoComment(String line) => line.contains('// TODO:');
